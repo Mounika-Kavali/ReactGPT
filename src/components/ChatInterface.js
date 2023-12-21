0e73183868b683f907f1f23fc95e18cb8907cc52
unknown <kavalimounika333@gmail.com>
2023-12-21 18:43:45 +0530

    chatGPT API is integrated with UI

--- src/components/ChatInterface.js
+++ src/components/ChatInterface.js
@@ -1,5 +1,6 @@
 // ChatInterface.js
 import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
 import SendIcon from "@material-ui/icons/Send";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import Microphone from "./Microphone";
@@ -52,11 +53,39 @@ const ChatInterface = () => {
     }
   };
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (inputText.trim() === "") return;
 
-    setMessages([...messages, { text: inputText, sender: "user" }]);
-    // You can make an API request here to ChatGPT and update the state with the response
+    const apiKey = "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";
+
+    try {
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: inputText }],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+
+      setMessages([
+        ...messages,
+        {
+          receiver: "user",
+          request: inputText,
+          response: response.data.choices[0].message.content,
+          sender: response.data.choices[0].message.role,
+        },
+      ]);
+    } catch (error) {
+      console.error("Error sending message:", error.message);
+    }
+
     setInputText("");
   };
 
@@ -86,20 +115,31 @@ const ChatInterface = () => {
         </button>
       </div>
       <div className="chat-messages" ref={messagesContainerRef}>
+        {console.log("messages", messages)}
         {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`message ${message.sender}`}
-            style={{
-              fontFamily: "sans-serif",
-              fontSize: "medium",
-            }}
-          >
-            {message.text.split("\n").map((line, i) => (
-              <div key={i} style={{ minHeight: "10px" }}>
-                {line}
-              </div>
-            ))}
+          <div key={index}>
+            <div
+              className={`message ${message.receiver}`}
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "medium",
+              }}
+            >
+              <div style={{ minHeight: "10px" }}>{message.request}</div>
+            </div>
+            <div
+              className={`message ${message.sender}`}
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "medium",
+              }}
+            >
+              {message.response.split("\n").map((line, i) => (
+                <div key={i} style={{ minHeight: "10px" }}>
+                  {line}
+                </div>
+              ))}
+            </div>
           </div>
         ))}
         {scrollBottomArrow && (
